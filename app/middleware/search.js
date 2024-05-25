18022f06dda40641805e92b504d1e3083f3b2683
mr0875 <enzoromerov@hotmail.com>
2024-05-25 07:58:11 -0300

    correccion de error de formato de los filtros

--- app/middleware/search.js
+++ app/middleware/search.js
@@ -2,25 +2,19 @@ import axios from 'axios';
 
 async function filterItems(searchQuery) {
   const searchResponse = await axios.get(`https://api.mercadolibre.com/sites/MLA/search?q=${searchQuery}`);
+  const sellerIds = searchResponse.data.results.slice(0, 4).map(item => item.seller.id);
+  console.log(sellerIds)
+  const locations = await Promise.all(sellerIds.map(getLocation));
   const formattedData = {
     author: {
-      name: 'MercadoLibre', // Reemplazar con el nombre real del autor si está disponible
+      name: 'MercadoLibre', 
       lastname: '',
     },
-    categories: async () => {
-      const firstCategoryId = searchResponse.data.results[0].category_id;
-      const categoryResponse = await axios.get(`https://api.mercadolibre.com/categories/${firstCategoryId}`);
-      const categoryNames = [];
-
-      for (const category of categoryResponse.data.path_from_root) {
-        categoryNames.push(category.name);
-      }
-
-      return categoryNames;
-    },
-    items: searchResponse.data.results.slice(0, 4).map((item) => ({
+    categories: await getCategories(searchResponse.data.results[0].category_id), // Llamar a una función separada para obtener las categorías
+    items: searchResponse.data.results.slice(0, 4).map((item, index) => ({
       id: item.id,
       title: item.title,
+      location: locations[index],
       price: {
         currency: item.currency_id,
         amount: item.available_quantity,
@@ -36,13 +30,14 @@ async function filterItems(searchQuery) {
 
 async function filterItemDetails(itemId) {
   const itemResponse = await axios.get(`https://api.mercadolibre.com/items/${itemId}`);
-  const descriptionResponse = await axios.get(`https://api.mercadolibre.com/items/${itemId}/description`);
-
+  const descriptionResponse = await axios.get(`https://api.mercadolibre.com/items/${itemResponse.data.id}/description`);
+  console.log(descriptionResponse.data)
   const formattedData = {
     author: {
-      name: 'MercadoLibre', // Reemplazar con el nombre real del autor si está disponible
+      name: 'MercadoLibre', 
       lastname: '',
     },
+    categories: await getCategories(itemResponse.data.category_id),
     item: {
       id: itemResponse.data.id,
       title: itemResponse.data.title,
@@ -51,7 +46,7 @@ async function filterItemDetails(itemId) {
         amount: itemResponse.data.price,
         decimals: itemResponse.data.decimal_price,
       },
-      picture: itemResponse.data.thumbnail,
+      picture: itemResponse.data.pictures[0].secure_url,
       condition: itemResponse.data.condition,
       free_shipping: itemResponse.data.shipping.free_shipping,
       sold_quantity: itemResponse.data.sold_quantity,
@@ -59,6 +54,19 @@ async function filterItemDetails(itemId) {
     },
   };
   return formattedData;
+}
+
+async function getCategories(categoryId) {
+  const categoryResponse = await axios.get(`https://api.mercadolibre.com/categories/${categoryId}`);
+  const categoryNames = [];
+  for (const category of categoryResponse.data.path_from_root) {
+    categoryNames.push(category.name);
+  }
+  return categoryNames;
+}
+async function getLocation(sellerId) {
+  const sellerResponse = await axios.get(`https://api.mercadolibre.com/users/${sellerId}`);
+  return sellerResponse.data.address.city;
 }
 
 export { filterItems, filterItemDetails }; 
